22f4213001270e194ce8b42993fd526a7d7c774f
ThinkHam <59174240+ThinkHam@users.noreply.github.com>
2022-05-29 21:19:31 -0700

    updated parsing with link support

--- src/main.py
+++ src/main.py
@@ -7,16 +7,16 @@ from bs4 import BeautifulSoup as bs
 from scipy import stats
 from re import sub
 
-products = {}
+products = []
 
 #url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw={keyword}&_sacat=0&_pgn={pagenum}'
 url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw={keyword}&_sacat=0&_ipg=240&_pgn={pagenum}&rt=nc&LH_BIN=1'
-test_keyword = 'ultrasound+textbook'
+test_keyword = 'jumbo+pikachu+yoda+plush'
 
 def main():
     
     #Scraping
-    for i in range(1,2):
+    for i in range(1,10):
         request = requests.get(url.format(keyword = test_keyword, pagenum = i))
         data = request.text
         soup = bs(data)
@@ -24,22 +24,27 @@ def main():
         for listing in listings:
             product_name = ''
             product_price = ''
+            product_link = ''
             for name in listing.find_all('h3', attrs={'class':'s-item__title'}):
                 if str(name.find(text=True, recursive=False))!='None':
                     product_name = str(name.find(text=True, recursive=False))
-                    products[product_name] = 0
             if product_name!='':
                 price = listing.find('span', attrs={'class':'s-item__price'})
                 product_price = str(price.find(text=True, recursive=False))
+                for a in listing.find_all('a', href=True, attrs={'class':'s-item__link'}):
+                    product_link = a['href']
                 try:
                     product_price = float(product_price[1:])
-                    if products[product_name] == 0:
-                        products[product_name] = product_price
+                    products.append({
+                        'Name' : product_name,
+                        'Price' : product_price,
+                        'Link' : product_link
+                    })
                 except ValueError:
                     continue
                         
         with open('results.json', 'w', encoding='utf-8') as f:
-            json.dump({key: val for key, val in sorted(products.items(), key = lambda ele: ele[1])}, f, ensure_ascii=False, indent=4)
+            json.dump(products, f, ensure_ascii=False, indent=4)
                 
     
     #Formatting
